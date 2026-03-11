14037649bfdea0b9d4034c0b7f82454fe394d21a
wn <lauweina@gmail.com>
2026-03-11 18:13:35 +0800

    Update attributes for Module Class to include prerequisite modules

--- src/main/java/Module.java
+++ src/main/java/Module.java
@@ -1,14 +1,30 @@
+import java.util.ArrayList;
+
 public class Module {
     private String moduleCode;
     private String moduleName;
+    private ArrayList<String> prerequisites;
 
     public Module(String moduleCode, String moduleName) {
         this.moduleCode = moduleCode;
         this.moduleName = moduleName;
+        this.prerequisites = new ArrayList<>();
+    }
+
+    public void addPrerequisite(String prereqCode) {
+        this.prerequisites.add(prereqCode);
     }
 
     @Override
     public String toString() {
         return moduleCode + " " + moduleName;
-     }
+    }
+
+    public String toStringPrerequisites() {
+        if (prerequisites.isEmpty()) {
+            return moduleCode + " has no prerequisites.";
+        }
+        return "Prerequisites for " + moduleCode +
+                ": " + String.join(", ", prerequisites);
+    }
 }
