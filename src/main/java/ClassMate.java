6f615a35071362acfd6887899c9d3e109aa90389
wn <lauweina@gmail.com>
2026-03-11 16:30:25 +0800

    Add print method to display a hardcoded list of CEG Major Requirements

--- src/main/java/ClassMate.java
+++ src/main/java/ClassMate.java
@@ -1,6 +1,27 @@
 public class ClassMate {
 
+    private static String listOfMajorRequirements = "MA1511 Engineering Calculus\n" +
+            "MA1512 Differential Equations for Engineering\n" +
+            "MA1508E Linear Algebra for Engineering\n" +
+            "EG2401A Engineering Professionalism\n" +
+            "CG1111A Engineering Principles and Practice I\n" +
+            "CG2111A Engineering Principles and Practice II\n" +
+            "CS1231 Discrete Structures\n" +
+            "CG2023 Signals & Systems\n" +
+            "CG2027 Transistor-level Digital Circuit\n" +
+            "CG2028 Computer Organization\n" +
+            "CG2271 Real-time Operating System\n" +
+            "CG3201 Machine Learning and Deep Learning\n" +
+            "CG3207 Computer Architecture\n" +
+            "CS2040C Data Structures and Algorithms\n" +
+            "CS2107 Introduction to Information Security\n" +
+            "CS2113 Software Engineering & Object-Oriented Programming\n" +
+            "EE2026 Digital Design\n" +
+            "EE4204 Computer Networks";
+
     public static void main (String args[]) {
-        System.out.println("ClassMate class serves as the main entry point for the CLI-based application ClassMate.");
+        System.out.println("Here is a list of modules required to fulfill the CEG Major: ");
+        System.out.println(listOfMajorRequirements);
+        System.out.println("\nNote: This list excludes Internship and FYP. ");
     }
 }
