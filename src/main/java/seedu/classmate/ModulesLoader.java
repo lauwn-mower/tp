b9bbe082fc14917d7dceb58ad10547f39327e3b7
VinayVR26 <133363378+VinayVR26@users.noreply.github.com>
2026-03-26 16:42:51 +0800

    Add method in ModulesLoader.java to load core modules information

--- src/main/java/seedu/classmate/ModulesLoader.java
+++ src/main/java/seedu/classmate/ModulesLoader.java
@@ -1,9 +1,9 @@
 package seedu.classmate;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 public class ModulesLoader {
     private static final String CORE_MODULES_PATH = "./data/core-modules.txt";
@@ -17,11 +17,46 @@ public class ModulesLoader {
 
         if ((coreModulesDirectory != null && !coreModulesDirectory.exists()) ||
                 (specialisationModulesDirectory != null && !specialisationModulesDirectory.exists())) {
-            throw new ClassMateException("The core modules and specialisation modules files are not exist");
+            throw new ClassMateException("The core modules and/or specialisation modules files do not exist");
         }
     }
 
-    public static void loadCoreModules() throws FileNotFoundException {
+    public static ArrayList<Module> loadCoreModules() throws ClassMateException {
+        ArrayList<Module> modules = new ArrayList<>();
         File file = new File(CORE_MODULES_PATH);
+
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine().trim();
+
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                String[] tokens = line.split(",");
+                // assert statement
+
+                String moduleName = tokens[0];
+                String moduleCode = tokens[1];
+                String semester = tokens[2];
+                String prerequisites = tokens[3];
+
+                Module module = new Module(moduleCode, moduleName);
+                module.setSemester(semester);
+
+                if (!prerequisites.equals("None")) {
+                    String[] prerequisiteCodes = prerequisites.split("\\|");
+                    for (String prerequisiteCode : prerequisiteCodes) {
+                        module.addPrerequisite(prerequisiteCode);
+                    }
+                }
+                modules.add(module);
+            }
+        } catch (FileNotFoundException e) {
+            throw new ClassMateException("The core modules files does not exist");
+        }
+
+        return modules;
     }
 }
